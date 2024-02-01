287e67f1c4c82a7b1027ebed01ab5746d2ebc025
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2024-02-01 11:03:38 +0300

    Add dependencies and update file paths

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -4,11 +4,13 @@ const courseController = require('../controllers/courseController')
 const router = express.Router();
 
 
-router.route('/').get(courseController.getAllCourses); 
-router.route('/').post(courseController.createCourse); 
 // https://localhost:3000/courses
+// router.route('/yeni').post(courseController.createCourse); --> https://localhost:3000/courses/yeni
 
-//router.route('/yeni').post(courseController.createCourse);
-// https://localhost:3000/courses/yeni
+
+router.route('/').post(courseController.createCourse); 
+router.route('/').get(courseController.getAllCourses); 
+router.route('/:slug').get(courseController.getCourse); 
+
 
 module.exports = router;
