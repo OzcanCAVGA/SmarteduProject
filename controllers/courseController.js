0437df6e55fe27e11d0280102093f4f74f1deb1a
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2024-02-10 17:43:56 +0300

    Fix bug in login functionality

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -3,11 +3,14 @@ const Category = require('../models/Category')
 
 exports.createCourse = async (req, res) => {
     try {
-        const course = await Course.create(req.body);
-        res.status(201).json({
-            status: 'success',
-            course
-        })
+        const course = await Course.create({
+            name: req.body.name,
+            description: req.body.name,
+            category: req.body.category,
+            user: req.session.userID
+
+        });
+        res.status(201).redirect(`/courses/${course.slug}`)
 
     } catch (error) {
         res.status(400).json({
@@ -24,7 +27,6 @@ exports.getAllCourses = async (req, res) => {
     try {
 
         const categorySlug = req.query.categories
-
         const category = await Category.findOne({ slug: categorySlug })
 
         let filter = {};
@@ -33,7 +35,7 @@ exports.getAllCourses = async (req, res) => {
             filter = { category: category._id }
         }
 
-        const courses = await Course.find(filter);
+        const courses = await Course.find(filter).sort("-createdAt");
         const categories = await Category.find();
 
         res.status(200).render('courses', {
